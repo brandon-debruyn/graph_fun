8d3f413a64ad5b14d0ee8940ac0c26d495bcf4d0
brandon-debruyn <brandondebruyn123@gmail.com>
2022-03-20 13:02:20 +0000

    square nodes and node states detection

--- graph_fun/src/App.js
+++ graph_fun/src/App.js
@@ -1,72 +1,104 @@
 import './index.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 // components 
 import SquareNodes from './components/SquareNodes';
 
+function App() {
 
-// initial grid to be generated
-const generateGrid = () => {
-  
-  const grid = [];
-  
-  for(let row=0; row < 25; row++) {
-    for(let col=0; col < 27; col++) {
-      // add node to current row -- (col, row) -> corresponds with x-axis (cols) / y-axis (rows) 
-      let key = col.toString() + row.toString()
-      grid.push(squareNodes(col, row, key));     
-
-    }
-  }
-
-  return grid;
-  
-}
-
-// individual nodes - returns each square's information as a object
-const squareNodes = (x, y, key) => {
-  return {
-    key: key, 
-    x: x,        // x coordinate
-    y: y,        // y coordinate
-  }
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      grid: [],
+  // hooks
+  // ----->
+  const [grid, setGrid] = useState(() => {
+    // individual nodes - returns each square's information as a object
+    const squareNode = (x, y) => {
+      return {
+        x: x,        // x coordinate
+        y: y,        // y coordinate
+        nodeType: 'none'
+      }
     }
 
-  }
+    const grid = [];
   
-  // lifecycle methods
-  componentDidMount() {
-    const grid = generateGrid();
+    for(let row=0; row < 20; row++) {
+      const rowElements = [];
+      for(let col=0; col < 50; col++) {
 
-    this.setState({grid});
+        // add node to current row array -- (col, row) -> corresponds with x-axis (cols) / y-axis (rows) 
+        rowElements.push(squareNode(col, row));
+      }
+
+      // add row array as row in grid
+      grid.push(rowElements);
+    }
+    return grid;
+  });
+
+  const[sourceNode, setSourceNode] = useState({});
+  const[destNode, setDestNode] = useState({});
+  const[nodeSelect, setNodeSelect] = useState('none');
+
+  // event handlers
+  function handleSetStartNode(e) {
+    e.preventDefault();
+    setNodeSelect('start');
   }
 
-  render() {
-    return(
-      <div className="graph">
-        <div className="grid-container">
-          {this.state.grid.map( (node) => {
-            return(
-              <SquareNodes
-                x={node.x}
-                y={node.y}
-                key={node.key}
-              />
-            );
-          })}
+  function handleSetDestNode(e) {
+    e.preventDefault();
+    setNodeSelect('dest');
+  }
+
+  function handleSetWallNodes(e) {
+    e.preventDefault();
+    setNodeSelect('wall');
+  }  
+
+  return(
+    <>
+      <div className="config-bar">
+        <div className="config-bar-container">
+          <header>
+            <h1>Algorithm Visualizer</h1>
+          </header>
+          <button className='run-algorithm'>Visualize Algorithm</button>
+          <div className='config-bar-settings'>
+            <button className='start-node' onClick={handleSetStartNode}>Set Start Node</button>
+            <button className='wall-nodes' onClick={handleSetWallNodes}>Set Wall Nodes</button>
+            <button className='end-node' onClick={handleSetDestNode}>Set End Node</button>
+          </div>
         </div>
       </div>
-    );
-  }
+      
+      <div className="grid">
+        <div className="grid-container">
+          {grid.map(
+            (rowNodes, rowIndex) => {
+              return(
+                rowNodes.map( (node, colIndex) => {
+                  return(
+                    <SquareNodes 
+                      key={rowIndex.toString() + colIndex.toString()}
+                      x={node.x}
+                      y={node.y}
+                      nodeType={node.nodeType}      
+                      grid={grid}  
+                      setGrid={setGrid}      
+                      sourceNode={sourceNode}
+                      setSourceNode={setSourceNode}
+                      destNode={destNode}
+                      setDestNode={setDestNode}            
+                      nodeSelect={nodeSelect}
+                      setNodeSelect={setNodeSelect}          
+                    />
+                  );
+                })
+              )}
+            )}
+        </div>
+      </div>
+    </>
+  );
 }
-
 
 export default App;
--- graph_fun/src/components/SquareNodes.js
+++ graph_fun/src/components/SquareNodes.js
@@ -1,15 +1,61 @@
 import React from 'react'
 
-class SquareNodes extends React.Component {
-    constructor(props) {
-        super(props);
+function SquareNodes(props) {
+
+    function setNode() {
+        
+        props.setGrid(prevGrid => {
+            const newGrid = [...prevGrid];
+
+            // set node type based on config selection
+            if(props.nodeSelect === 'start') {
+                newGrid[props.y][props.x].nodeType = 'start';
+                props.setSourceNode(prevSourceNode => ({
+                    ...prevSourceNode, 
+                    x: {
+                        ...prevSourceNode.x,
+                        x: props.x
+                    },
+                    y: {
+                        ...prevSourceNode.y,
+                        y: props.y
+                    }
+                }));
+            }
+            
+            if(props.nodeSelect === 'dest') {
+                newGrid[props.y][props.x].nodeType = 'dest';
+                props.setDestNode(prevDestNode => ({
+                    ...prevDestNode, 
+                    x: {
+                        ...prevDestNode.x,
+                        x: props.x
+                    },
+                    y: {
+                        ...prevDestNode.y,
+                        y: props.y
+                    }
+                }));
+            }
+            
+            if(props.nodeSelect === 'wall') {
+                newGrid[props.y][props.x].nodeType = 'wall';
+
+            }
+
+            return newGrid;
+        })
     }
 
-    render() {
-        return(
-            <div className="squareNode"></div>
-        );
-    }
+    return(
+        <div 
+            className="squareNode"
+            id={props.x + "-" + props.y}
+            onClick={() => {setNode()}}
+        ></div>
+    );
 }
+
+
 
 export default SquareNodes;
