1f113665733597aff73c6fd4232e97d47bc3ede2
brandon-debruyn <brandondebruyn123@gmail.com>
2022-05-29 20:25:53 +0100

    new neighbours

--- graph_fun/src/App.js
+++ graph_fun/src/App.js
@@ -9,7 +9,6 @@ import breadth_first_search from './algorithms/breadth_first_search';
 const NO_GRID_ROWS = 20;
 const NO_GRID_COLS = 50;
 
-
 function App() {
 
   // hooks
@@ -17,142 +16,30 @@ function App() {
   const [grid, setGrid] = useState(() => {
     // individual nodes - returns each square's information as a object
     const squareNode = (x, y) => {
-
+      let adjList = []
       /* more efficient fix for getNeigbours required ---> tbd */
       const getNeigbours = (x, y) => {
+        let dirs = [
+          [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1],
+          [1, 0], [1, 1],
+          ]
+        
+        for(let i=0; i<dirs.length; i++) {
+          nx = x + dirs[i][0]
+          ny = y + dirs[i][1]
 
-        // corner nodes
-        if(x === 0 && y === 0) {
-          return([{
-            adjX: x + 1,
-            adjY: y
-          },
-          {
-            adjX: x,
-            adjY: y + 1
-          }]);
-        } 
-        else if(x === 0 && y === (NO_GRID_ROWS - 1)) {
-          return([{
-            adjX: x + 1,
-            adjY: y
-          },
-          {
-            adjX: x,
-            adjY: y - 1
-          }]);
-        }  
-        else if(x === (NO_GRID_COLS - 1) && y === 0) {
-          return([{
-            adjX: x - 1,
-            adjY: y
-          },
-          {
-            adjX: x,
-            adjY: y + 1
-          }]);
-        }
-        else if(x === (NO_GRID_COLS - 1) && y === (NO_GRID_ROWS - 1)) {
-          return([{
-            adjX: x - 1,
-            adjY: y
-          },
-          {
-            adjX: x,
-            adjY: y - 1
-          }]);
-        }
-        // left and right side nodes excluding corners
-        if( (x === 0 || x === (NO_GRID_COLS - 1) ) && 
-          ( y !== 0 || y !== (NO_GRID_ROWS - 1)) ) {
-
-          // left node's neighbours
-          if(x === 0) {
-            return([{
-              adjX: x + 1,
-              adjY: y
-            },
-            {
-              adjX: x,
-              adjY: y + 1
-            },
-            {
-              adjX: x,
-              adjY: y - 1
-            }]);
-          }
-          // right node's neighbours ( x === NO_GRID_COLS - 1 )
-          else {
-            return([{
-              adjX: x - 1,
-              adjY: y
-            },
-            {
-              adjX: x,
-              adjY: y + 1
-            },
-            {
-              adjX: x,
-              adjY: y - 1
-            }]);
-          }
-        }
-        // top and bottom side nodes excluding corners
-        else if( (y === 0 || y === (NO_GRID_ROWS - 1) ) && 
-                ( x !== 0 || x !== (NO_GRID_COLS - 1)) ) {
-          // top node's neighbours
-          if(y === 0) {
-            return([{
-              adjX: x,
-              adjY: y + 1
-            },
-            {
-              adjX: x + 1,
-              adjY: y
-            },
-            {
-              adjX: x - 1,
-              adjY: y
-            }]);
-          }
-          // bottom node's neighbours ( y === NO_GRID_ROWS - 1 )
-          else {
-            return([{
-              adjX: x,
-              adjY: y - 1
-            },
-            {
-              adjX: x + 1,
-              adjY: y
-            },
-            {
-              adjX: x - 1,
-              adjY: y
-            }]);
-          }
-        } 
-        // not top/bot or left/right side nodes or corner nodes - (each node gets 4 neighbours)
-        else {
-          return([{
-            adjX: x + 1,   // above node
-            adjY: y
-          },
-          {
-            adjX: x - 1,   // beneath node
-            adjY: y
-          },
-          {
-            adjX: x,
-            adjY: y + 1    // right neighbour
-          },
-          {
-            adjX: x,
-            adjY: y - 1    // left neighbour
-          }]);
+          if( !(0 <= nx <= NO_GRID_COLS) && !(0 <= ny <= NO_GRID_ROWS) ) {
+            continue
+          }        
+          if(nodeType == 'wall') {
+            continue
+          }  
+          
+          adjList.push([nx, ny])
         }
       }
 
-      let adjList = getNeigbours(x, y);
+      adjList = getNeigbours(x, y)      
 
       return {
         x: x,               // x coordinate
